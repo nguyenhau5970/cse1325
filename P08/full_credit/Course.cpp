e0064b28f6273ee36d374afde6b08e91e3b25132
Nguyen Hau <nguyen.hau@mavs.uta.edu>
2021-04-13 08:09:59 -0500

    second commit

--- P08/full_credit/Course.cpp
+++ P08/full_credit/Course.cpp
@@ -1,11 +1,18 @@
 #include "Course.h"
 
 #include <fstream>
+#include <iostream>
 
 Course::Course(std::istream ist){
     std::getline(ist,_subject);
+    std::getline(ist,_grade;
 }
 
 void Course::save(std::ostream& ost) {
-    ost << _subject << ", (" << _grade << ")";
+    ost << _subject << "\n" << _grade << "\n";
+}
+
+std::ostream& operator<<(std::ostream& ost, const Course& course) {
+    ost << course._subject << " (grade " << course._grade << ")";
+    return ost;
 }
